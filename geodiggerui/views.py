5fbb613f3beab8cef19151deb2eac92b0c9b6b31
george <rpubaddr0@gmail.com>
2014-03-14 04:35:55 -0600

    Wrap it in a datetime.

--- geodiggerui/views.py
+++ geodiggerui/views.py
@@ -30,7 +30,8 @@ class GeoDiggerUI(object):
 
         # Read settings from the server.
         # TODO: Make this smarter.
-        first = [r for r in self.db.find().limit(1)][0]['time']
+        first = datetime.datetime.strptime([r for r in self.db.find().limit(1)][0]['time'],
+                "%Y-%m-%d %H:%M:%S")
         self.minDate = "2012, 0, 1" % (first.year, first.month-1, first.day)
         now = datetime.datetime.now()
         self.maxDate = "%s, %s, %s" % (now.year, now.month-1, now.day)
